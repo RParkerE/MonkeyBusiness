ce75da80d440a45e0d6cf5dab458e44a3d560dc4
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2024-01-31 02:18:54 -0600

    Add VM & Compiler for Monkey lang

---
+++ GoCompiler/evaluator/builtins.go
@@ -0,0 +1,19 @@
+package evaluator
+
+import (
+	"GoCompiler/object"
+)
+
+var builtins = map[string]*object.Builtin{
+	"len": object.GetBuiltinByName("len"),
+
+	"first": object.GetBuiltinByName("first"),
+
+	"last": object.GetBuiltinByName("last"),
+
+	"rest": object.GetBuiltinByName("rest"),
+
+	"push": object.GetBuiltinByName("push"),
+
+	"puts": object.GetBuiltinByName("puts"),
+}
